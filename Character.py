1db0c70d89c637500ae208c18203f509a2ce733f
Giruvagen <40126586+Giruvagen@users.noreply.github.com>
2018-07-09 22:52:40 +0100

    tidying up

--- Character.py
+++ Character.py
@@ -1,26 +1,46 @@
 #Init character with initial values
 import random
+import time
 class Character(object):
     def __init__(self,name):
       self.name = name
-    def startroll(self):
+    def startGame(self):
+        print("Let the games begin!")
+    def startRoll(self):
         print("Please select your intial statistics. Please note you may not set a single item to higher than 4 and the total of all 3 must be less than 10")
         atts = []
-        self.st = int(input("What is your strength?"))
+        self.st = int(input("What is your strength? "))
         atts.append(self.st)
-        self.defe = int(input("What is your defence?"))
-        atts.append(self.defe)
-        self.spd = int(input("What is your speed?"))
-        atts.append(self.spd)
-        if atts[0] >= 5 or atts[1] >= 5 or atts[2] >= 5:
+        time.sleep(2)
+        if atts[0]:
           print("The number you entered, is too damn high!")
-        elif sum(atts) > 10:
-          print("The total you entered, is too damn high!")
+          time.sleep(3)
+        self.defe = int(input("What is your defence? "))
+        atts.append(self.defe)
+        time.sleep(2)
+        if atts[1] >= 5:
+          print("The number you entered, is too damn high!")
+          time.sleep(3)
+        self.spd = int(input("What is your speed? "))
+        atts.append(self.spd)
+        time.sleep(2)
+        if atts[2] >= 5:
+          print("The number you entered, is too damn high!")
+          time.sleep(3)
+        if sum(atts) > 10:
+          print("The total you entered, is too damn high! Try Again!")
+          time.sleep(3)
+          self.startRoll()
         else:
           print("Thank you.")
+          time.sleep(5)
           self.st = atts[0] + random.randint(0,5)
           self.defe = atts[1] + random.randint(0,5)
           self.spd = atts[2] + random.randint(0,5)
           print("Your strength is {:d}".format(self.st))
+          time.sleep(1)
           print("Your defence is {:d}".format(self.defe))
+          time.sleep(1)
           print("Your speed is {:d}".format(self.spd))
+          time.sleep(1)
+          self.startGame()
