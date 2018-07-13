c9f600d5f805f778cbd6b1c79538c292433e7662
Giruvagen <40126586+Giruvagen@users.noreply.github.com>
2018-07-13 23:15:58 +0100

    added map overlap added battle start conditon

--- Moves.py
+++ Moves.py
@@ -5,19 +5,50 @@ class motion(object):
 
     def __init__(self, char):
       self.char = char
+    def battle(self):
+        print("You can't fight yet!")
+        self.move(self.direction)
     def move(self, direction):
         if self.direction == "north":
             self.starty += 1
-            print(mapdesc[(self.startx,self.starty)])
+            if self.starty > 2:
+                self.starty = 0
+                print(mapdesc[(self.startx,self.starty)])
+            else:
+              print(mapdesc[(self.startx,self.starty)])
+              self.battlechance = random.randint(0,8)
+              if self.battlechance == 1:
+                self.battle()
         elif self.direction == "south":
             self.starty -= 1
-            print(mapdesc[(self.startx,self.starty)])
+            if self.starty < 0:
+                self.starty = 2
+                print(mapdesc[(self.startx,self.starty)])
+            else:
+              print(mapdesc[(self.startx,self.starty)])
+              self.battlechance = random.randint(0,8)
+              if self.battlechance == 1:
+                self.battle()
         elif self.direction ==  "east":
             self.startx += 1
-            print(mapdesc[(self.startx,self.starty)])
+            if self.startx > 2:
+                self.startx = 0
+                print(mapdesc[(self.startx,self.starty)])
+            else:
+              print(mapdesc[(self.startx,self.starty)])
+              self.battlechance = random.randint(0,8)
+              if self.battlechance == 1:
+                self.battle()
         elif self.direction == "west":
             self.startx -= 1
-            print(mapdesc[(self.startx,self.starty)])
+            if self.startx < 0:
+                self.startx = 2
+                print(mapdesc[(self.startx,self.starty)])
+            else:
+              print(mapdesc[(self.startx,self.starty)])
+              self.battlechance = random.randint(0,8)
+              if self.battlechance == 1:
+                self.battle()
         self.direction = input("Choose your direction of travel: ")
         self.move(self.direction)
     def startmove(self):
