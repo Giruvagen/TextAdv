7de87d845a4073d80a2878c1d1376cb05a559266
Giruvagen <40126586+Giruvagen@users.noreply.github.com>
2018-07-14 13:32:09 +0100

    starter inherits from moves, character

--- starter.py
+++ starter.py
@@ -5,6 +5,7 @@ class starter(Character, Moves):
         def startGame(self):
           self.char = input("What is your name? ")
           print(self.char)
-          player.startRoll()
+          self.startRoll()
 
-starter.startGame()
+player = starter()
+player.startGame()
